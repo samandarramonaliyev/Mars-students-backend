93f2163bd8bc373af1862c2d5b28164cf094973f
samandar-pixel <samandarrahmonaliev3005@gmail.com>
2026-01-31 01:22:33 +0500

    coin system effect

--- marsdevs/asgi.py
+++ marsdevs/asgi.py
@@ -4,17 +4,21 @@ ASGI config for marsdevs project.
 
 import os
 
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marsdevs.settings')
 
-django_asgi_app = get_asgi_application()
+# Явно инициализируем Django перед импортом приложений
+django.setup()
 
 # Импорты после инициализации Django, чтобы приложения были загружены
 from api.routing import websocket_urlpatterns  # noqa: E402
 from api.ws_auth import JwtAuthMiddleware  # noqa: E402
+
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
